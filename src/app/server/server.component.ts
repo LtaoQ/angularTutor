35c5197b03ef4882a9bd7ce47c5e14858cf4b4dd
litao <litaoqiu@gmail.com>
2019-07-27 23:20:24 -0500

    online order feature completed

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,8 +8,80 @@ import { Student } from '../../shared/student.model';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+
+    isBun = false;
+    isPatty = false;
+    isChicken = false;
+    isCheese = false;
+    isBacon = false;
+    isLnT = false;
+    isServe = false;
+    serveMsg = "nothing to serve";
+    clickBun() {
+      this.isBun = !this.isBun;
+      this.isServe = false;
+    }
+
+    clickPatty() {
+      this.isPatty = !this.isPatty;
+      this.isServe = false;
+    }
+
+    clickChicken() {
+      this.isChicken = !this.isChicken;
+      this.isServe = false;
+    }
+
+    clickCheese() {
+      this.isCheese = !this.isCheese;
+      this.isServe = false;
+    }
+
+    clickBacon() {
+      this.isBacon = !this.isBacon;
+      this.isServe = false;
+    }
+
+    clickLnT() {
+      this.isLnT = !this.isLnT;
+      this.isServe = false;
+    }
+
+    clickClear() {
+      this.isServe = false;
+      this.isBun = false;
+      this.isPatty = false;
+      this.isChicken = false;
+      this.isCheese = false;
+      this.isBacon = false;
+      this.isLnT = false;
+    }
+
+    clickSubmit() {
+
+      if (!this.isBun) {
+        this.serveMsg = 'no bun!!!!';
+      } else if((!this.isPatty && !this.isChicken && !this.isCheese)) {
+        this.serveMsg = 'no meat or cheese!';
+      } else if (!this.isPatty && !this.isChicken) {
+        this.serveMsg = (this.isBacon ? 'bacon' : '') + ' grilled cheese ' + (this.isLnT ? 'with lettuce and tomato' : '');
+      } else if (this.isPatty && this.isChicken) {
+        this.serveMsg = 'chicken and beef? don\'t be too greedy, eh?';
+      } else {
+        this.serveMsg = (this.isBacon ? 'bacon' : '') +
+           (this.isCheese ? ' cheese' : '') + (this.isPatty ? ' burger' : ' chicken sandwich')+
+           (this.isLnT ? ' with lettuce and tomato' : '');
+      }
+
+
+
+      this.isServe = true;
+    }
+
+
     serverId = 10;
     serverStatus = 'offLine';
+    magicname = 'hahaha';
     counter = 0;
     isLogin = false;
     userName: string;
